23bbc6e9b592a73e2598035b4fcca39083c93f7c
Samuel W Linton <samuelwlinton@gmail.com>
2024-12-26 18:31:15 -0800

    Add Robot Car

--- Devices/RCCar/rc_car.py
+++ Devices/RCCar/rc_car.py
@@ -53,7 +53,7 @@ class RCCar:
         
 
 if __name__ == '__main__':
-    rc_car = RCCar(led_pin=9, buzzer_pin=22)
+    rc_car = RCCar(led_pin=6, buzzer_pin=22)
     asyncio.run(rc_car.start())
     
         
