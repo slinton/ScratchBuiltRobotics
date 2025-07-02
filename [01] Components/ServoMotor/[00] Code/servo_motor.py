a1c8ba1410b538aa5ba4641e7b7c04102a73061d
Samuel W Linton <samuelwlinton@gmail.com>
2025-07-01 20:53:09 -0700

    Start ServoMotor base class concept

---
+++ [01] Components/ServoMotor/[00] Code/servo_motor.py
@@ -0,0 +1,130 @@
+#
+# ServoMotor
+#
+# Version: 1.00
+# Date: 2025-07-01
+# Author: Sam Linton
+# Description: Control of servo motor 
+#
+# Note: This will replace Servo, ServoInfo, and ServoBase
+#
+# raw = sign * angle + raw_angle_0
+# angle = sign * (raw - raw_angle_0)
+#
+class ServoMotor:
+    def __init__(self, 
+                 name: str ='',
+                 pin: int = 0,
+                 raw_angle_0: float = 0.0, # Raw angle corresponding to angle = 0.0
+                 angle_start: float = 0.0, # Logical starting angle
+                 angle_end: float = 180.0, # Logical ending value
+                 angle_home: float = 90.0, # Logical home angle
+                 min_us: float = 544.0, # Or 600.0
+                 max_us: float = 2400.0,
+                 freq: int = 50 # PWM frequency in Hz
+                 ) -> None:
+        self._name: str = f'Servo: {str(pin)}' if name == '' else name
+        self._pin: int = pin 
+
+        self._initialized: bool = False # angle is not initialized
+        self._angle: float = angle_home
+        self._raw_angle_0: float = raw_angle_0
+        self._sign: int = 1 if angle_end > angle_start else -1
+
+        self._angle_start: float = angle_start
+        self._angle_end: float = angle_end
+        self._angle_home: float = angle_home
+
+        self._min_us: float = min_us
+        self._max_us: float = max_us
+        self._freq: int = freq
+
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def pin(self) -> int:
+        return self._pin
+    
+    @property
+    def angle(self) -> float:
+        if not self._initialized:
+            raise ValueError("Angle not initialized.")
+        return self._angle
+    
+    def read_angle(self) -> float:
+        """Read the logical angle from the servo."""
+        raw_angle = self._read_raw_angle()
+        self._angle = self._angle_from_raw_angle(raw_angle)
+        self._initialized = True
+        return self._angle
+    
+    def _read_raw_angle(self) -> float:
+        """Read the servo angle from the hardware."""
+        raise NotImplementedError("This method should be overridden in subclasses.")
+    
+    def write_angle(self, angle: float) -> None:
+        """Write logical angle to the servo."""
+        raw_angle = self._raw_angle_from_angle(angle)
+        if not self.angle_in_range(angle):
+            raise ValueError(f'Angle {angle} is out of range ({self._angle_start}, {self._angle_end})')
+        self._write_raw_angle(raw_angle)
+        self._angle = angle
+        self._initialized = True
+    
+    def _write_raw_angle(self, raw_angle: float) -> None:
+        """Write the servo angle to the hardware."""
+        raise NotImplementedError("This method should be overridden in subclasses.")
+    
+    # TODO: Add an async version of this method
+    def move_to_angle(self, angle: float, time: float = 0.0, angle_inc: float = 1.0) -> None:
+        current_angle = self.read_angle()
+        num_steps: int = int(abs((angle - current_angle) / angle_inc)) # Counts now 
+        
+        # Immediate move if no time
+        if time <= 0.0 or num_steps <= 1:
+            self.write_angle(angle)
+            return
+        
+        # Move in steps
+        angle_inc = (angle - current_angle) / num_steps
+        time_inc = time / (num_steps - 1)
+        
+        t = 0.0
+        for n in range(num_steps):
+            new_angle: float = current_angle + (n + 1) * angle_inc
+            print(f'Moving to angle: {new_angle} (step {n}/{num_steps})')
+            self.write_angle(new_angle)
+            if n < num_steps -1: 
+                self._sleep(time_inc)
+                t += time_inc
+            print(f'Angle {new_angle} at time {t:.2f} seconds')
+
+    def _sleep(self, seconds: float) -> None:
+        pass
+    
+    def angle_in_range(self, angle: float) -> bool:
+        """True if the logical angle is within the allowed range."""
+        return  (self._angle_end - angle) * (angle - self._angle_start) >= 0.0
+    
+    def _raw_angle_from_angle(self, angle: float) -> float:
+        """Get the servo angle from the logical angle"""
+        return self._raw_angle_0 + self._sign * angle
+    
+    def _angle_from_raw_angle(self, raw_angle: float) -> float:
+        return self._sign * (raw_angle - self._raw_angle_0)
+    
+    def __str__(self) -> str:
+        return f'{self._name} at pin {self._pin}\n' + \
+            f'Angle: {self._angle}\n' + \
+            f'Angle Range: {self._angle_start}-{self._angle_end}\n' + \
+            f'Home Angle: {self._angle_home}\n' + \
+            f'Zero Raw Angle = {self._raw_angle_0}'
+    
+   
+if __name__ == "__main__":
+    # Test code
+    servo_motor = ServoMotor(pin=2, angle_start=0, angle_end=180, name='Servo 2')
+    print(servo_motor)
+
---
+++ [01] Components/ServoMotor/[00] Code/servo_motor_mock.py
@@ -0,0 +1,72 @@
+#
+# ServoMotorMock
+#
+# Version: 1.00
+# Date: 2025-07-01
+# Author: Sam Linton
+# Description: Mock implementation of ServoMotor for testing purposes.
+#
+#
+from typing import override
+from servo_motor import ServoMotor
+class ServoMotorMock(ServoMotor):
+    """Mock implementation of ServoMotor for testing purposes."""
+    
+    def __init__(self, 
+                 name: str = 'ServoMotorMock',
+                 pin: int = 0,
+                 raw_angle_0: float = 0.0,
+                 angle_start: float = 0.0,
+                 angle_end: float = 180.0,
+                 angle_home: float = 90.0,
+                 min_us: float = 544.0,
+                 max_us: float = 2400.0,
+                 freq: int = 50
+                 ) -> None:
+        super().__init__(name, pin, raw_angle_0, angle_start, angle_end, angle_home, min_us, max_us, freq)
+        self._angle = angle_home  # Initialize to home position
+
+    @override
+    def _read_raw_angle(self) -> float:
+        print("[Mock] Reading raw angle")
+        return self._raw_angle_from_angle(self._angle)
+
+    @override
+    def _write_raw_angle(self, raw_angle: float) -> None:
+        print(f"[Mock] Writing raw angle: {raw_angle}")
+
+    @override
+    def _sleep(self, seconds: float) -> None:
+        import time
+        print(f"[Mock] Sleeping for {seconds} seconds")
+        time.sleep(seconds)
+
+
+if __name__ == '__main__':
+    # Example usage
+    servo = ServoMotorMock(
+        name='TestServo', 
+        pin=1, 
+        raw_angle_0=90.0, 
+        angle_start=-45.0, 
+        angle_end=45.0,
+        angle_home=0.0)
+    
+    try:
+        print(servo.angle)
+    except ValueError as e:
+        print(f'Error: {e}')
+    
+    print(f'{servo}\n')
+
+    print('\nTest write and read angle:')
+    angle = -30.0
+    print(f'write_angle: {angle}')
+    servo.write_angle(angle)
+    print(f'Angle after write: {servo.read_angle()}')
+
+    print('\nTest move to angle:')
+    new_angle = 30.0
+    print(f'Moving to angle: {new_angle}')
+    servo.move_to_angle(new_angle, time=6.0, angle_inc=10)
+    print(f'Angle after move: {servo.read_angle()}')
